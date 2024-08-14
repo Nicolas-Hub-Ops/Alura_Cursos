9596cf79c1b5cefbbd68cd99f6d7236b4ae5361a
Nicolas Silva Canabarro <nicolasnilo14@gmail.com>
2024-08-14 16:57:33 -0300

    Styled Components

--- Estudos/Front End/React JS/React - desenvolvendo em React Router com JavaScript/src/App.js
+++ Estudos/Front End/React JS/React - desenvolvendo em React Router com JavaScript/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/sobreMim";
 
 function App() {
   return (
     <div className="App">
-        
+      <BrowserRouter>
+        <Link to="/">Inicio</Link>
+        <Link to="/sobremim">Sobre Mim</Link>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
